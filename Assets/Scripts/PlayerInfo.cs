76622d663ee6e32384ada64f296216b6891b67c8
Ben <boriold64@gmail.com>
2018-04-03 16:00:06 -0400

    Added PlayerInfo Script
    Script is for saving player info data across scenes. Can be added to as necessary as more player variables arise.

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerInfo {
+
+    public static int gold;
+    public static GameObject[] soldiers;
+
+    public static int Gold {
+        get {
+            return gold;
+        }
+        set {
+            gold = value;
+        }
+    }
+
+    public static GameObject[] Soldiers {
+        get {
+            return soldiers;
+        }
+        set {
+            soldiers = value;
+        }
+    }
+}
