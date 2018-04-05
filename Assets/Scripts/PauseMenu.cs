683e0a2b0856866289ee2199b00ae8511879cb0a
valtiewa <valtiewa>
2018-04-05 19:25:38 -0400

    Added buttons to pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SaveGame()
+    {
+        //Save scene so that it can be loaded later
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
