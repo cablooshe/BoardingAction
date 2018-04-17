446b08c88be25c22d90b4cdaebec945ad389cfb5
Jake Douglass <douglaje@rose-hulman.edu>
2018-04-17 11:52:59 -0400

    Wall Prefabs

---
+++ Assets/__Scripts/Structure.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public float durability = 50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
