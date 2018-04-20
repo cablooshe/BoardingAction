a7a5eb7fc3541541809c36457cc4c774836ace78
soehrejm <soehrejm@rose-hulman.edu>
2018-04-20 16:34:48 -0400

    Doors open/close over time

--- Assets/__Scripts/Map Scripts/DoubleDoor.cs
+++ Assets/__Scripts/Map Scripts/DoubleDoor.cs
@@ -9,10 +9,20 @@ public class DoubleDoor : MonoBehaviour {
 
 	[Header("Set Dynamically")]
 	public bool				isOpen = false;		// Is the door currently open?
+	public bool				opening = false; 	// Is the door in the middle of opening?
+	public bool				closing = false;	// Is the door in the middle of closing?
 
 	public GameObject 		door1; 				// Lower x/y-positioned door
 	public GameObject 		door2; 				// Higher x/y-positioned door
-	public float			closeTime;			// Time to close the doors
+
+	public float			startOpen;			// When the doors started opening
+	public float			finishOpen;			// Time for the doors to be completely open
+	public float			startClose;			// When the doors started closing
+	public float 			finishClose; 		// Time for the doors to be completely closed
+	public float			closeTime;			// Time to start closing the doors
+
+	public float			startPos1;			// Starting position for door1
+	public float			startPos2;			// Starting position for door2
 
 	void Awake() {
 		door1 = transform.GetChild (0).gameObject;
@@ -24,6 +34,8 @@ public class DoubleDoor : MonoBehaviour {
 				door2 = door1;
 				door1 = temp;
 			}
+			startPos1 = door1.transform.position.y;
+			startPos2 = door2.transform.position.y;
 		} else {
 			if (door1.transform.position.x < temp.transform.position.x)
 				door2 = temp;
@@ -31,40 +43,59 @@ public class DoubleDoor : MonoBehaviour {
 				door2 = door1;
 				door1 = temp;
 			}
+			startPos1 = door1.transform.position.x;
+			startPos2 = door2.transform.position.x;
 		}
 		closeTime = Time.time;
 	}
 
 	void Update() {
+		if (opening) {
+			float openStatus = Mathf.Min (Time.time - startOpen, 1);
+			if (isVertical) {
+				door1.transform.position = new Vector3 (door1.transform.position.x, startPos1 - openStatus, door1.transform.position.z);
+				door2.transform.position = new Vector3 (door2.transform.position.x, startPos2 + openStatus, door2.transform.position.z);
+			} else {
+				door1.transform.position = new Vector3 (startPos1 - openStatus, door1.transform.position.y, door1.transform.position.z);
+				door2.transform.position = new Vector3 (startPos2 + openStatus, door2.transform.position.y, door2.transform.position.z);
+			}
+			if (Time.time > finishOpen) {
+				opening = false;
+				closeTime = Time.time + 5;
+			}
+		} else if (closing) {
+			float closeStatus = Mathf.Max (finishClose - Time.time, 0);
+			if (isVertical) {
+				door1.transform.position = new Vector3 (door1.transform.position.x, startPos1 - closeStatus, door1.transform.position.z);
+				door2.transform.position = new Vector3 (door2.transform.position.x, startPos2 + closeStatus, door2.transform.position.z);
+			} else {
+				door1.transform.position = new Vector3 (startPos1 - closeStatus, door1.transform.position.y, door1.transform.position.z);
+				door2.transform.position = new Vector3 (startPos2 + closeStatus, door2.transform.position.y, door2.transform.position.z);
+			}
+			if (Time.time > finishClose) {
+				closing = false;
+			}
+		}
 		if (isOpen && (Time.time > closeTime)) {
 			CloseDoors ();
 		}
 	}		
 
 	public void OpenDoors() {
-		if (isOpen)
+		if (isOpen || opening || closing)
 			return;
-		if (isVertical) {
-			door1.transform.position = new Vector3 (door1.transform.position.x, door1.transform.position.y - 1, door1.transform.position.z);
-			door2.transform.position = new Vector3 (door2.transform.position.x, door2.transform.position.y + 1, door2.transform.position.z);
-		} else {
-			door1.transform.position = new Vector3 (door1.transform.position.x - 1, door1.transform.position.y, door1.transform.position.z);
-			door2.transform.position = new Vector3 (door2.transform.position.x + 1, door2.transform.position.y, door2.transform.position.z);
-		}
 		isOpen = true;
-		closeTime = Time.time + 5;
+		opening = true;
+		startOpen = Time.time;
+		finishOpen = Time.time + 1;
 	}
 
 	public void CloseDoors() {
-		if (!isOpen)
+		if (!isOpen || closing || opening)
 			return;
-		if (isVertical) {
-			door1.transform.position = new Vector3 (door1.transform.position.x, door1.transform.position.y + 1, door1.transform.position.z);
-			door2.transform.position = new Vector3 (door2.transform.position.x, door2.transform.position.y - 1, door2.transform.position.z);
-		} else {
-			door1.transform.position = new Vector3 (door1.transform.position.x + 1, door1.transform.position.y, door1.transform.position.z);
-			door2.transform.position = new Vector3 (door2.transform.position.x - 1, door2.transform.position.y, door2.transform.position.z);
-		}
 		isOpen = false;
+		closing = true;
+		startClose = Time.time;
+		finishClose = Time.time + 1;
 	}
 }
