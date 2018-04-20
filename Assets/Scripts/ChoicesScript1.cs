f024bc822699356ed4ba40bd80617eec395f5a1c
valtiewa <valtiewa>
2018-04-20 14:46:57 -0400

    New Button Script
    created a script to change the text in the buttons and pull data into the game.

---
+++ Assets/Scripts/ChoicesScript1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoicesScript1 : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public string changedText;
+    //Only for changing text of a button
+
+    public Button buttonclicked;
+    public Text textTo;
+    public int ChoiceMade;
+
+    public GameObject Result01;
+
+
+    public void changeTheText()
+    {
+        ChoiceMade = 1;
+
+    }
+
+   
+    // Update is called once per frame
+    void Update () {
+        if (ChoiceMade == 1)
+        {
+            buttonclicked = GetComponent<Button>();
+            foreach (Transform t in transform)
+            { // might not be obvious
+                textTo = t.GetComponent<Text>();
+            }
+            buttonclicked.interactable = false;
+            //buttonclicked = "Hello!";
+        }
+
+        ChoiceMade = 0;
+    }
+}
