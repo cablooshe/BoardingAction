958f28d54b9d73c114fd98c22171c967d895978d
soehrejm <soehrejm@rose-hulman.edu>
2018-04-20 15:55:49 -0400

    Closing Doors
    Implemented CloseDoors() in DoubleDoor

--- Assets/__Scripts/Map Scripts/DoubleDoor.cs
+++ Assets/__Scripts/Map Scripts/DoubleDoor.cs
@@ -12,6 +12,7 @@ public class DoubleDoor : MonoBehaviour {
 
 	public GameObject 		door1; 				// Lower x/y-positioned door
 	public GameObject 		door2; 				// Higher x/y-positioned door
+	public float			closeTime;			// Time to close the doors
 
 	void Awake() {
 		door1 = transform.GetChild (0).gameObject;
@@ -31,7 +32,14 @@ public class DoubleDoor : MonoBehaviour {
 				door1 = temp;
 			}
 		}
+		closeTime = Time.time;
 	}
+
+	void Update() {
+		if (isOpen && (Time.time > closeTime)) {
+			CloseDoors ();
+		}
+	}		
 
 	public void OpenDoors() {
 		if (isOpen)
@@ -44,5 +52,19 @@ public class DoubleDoor : MonoBehaviour {
 			door2.transform.position = new Vector3 (door2.transform.position.x + 1, door2.transform.position.y, door2.transform.position.z);
 		}
 		isOpen = true;
+		closeTime = Time.time + 5;
+	}
+
+	public void CloseDoors() {
+		if (!isOpen)
+			return;
+		if (isVertical) {
+			door1.transform.position = new Vector3 (door1.transform.position.x, door1.transform.position.y + 1, door1.transform.position.z);
+			door2.transform.position = new Vector3 (door2.transform.position.x, door2.transform.position.y - 1, door2.transform.position.z);
+		} else {
+			door1.transform.position = new Vector3 (door1.transform.position.x + 1, door1.transform.position.y, door1.transform.position.z);
+			door2.transform.position = new Vector3 (door2.transform.position.x - 1, door2.transform.position.y, door2.transform.position.z);
+		}
+		isOpen = false;
 	}
 }
