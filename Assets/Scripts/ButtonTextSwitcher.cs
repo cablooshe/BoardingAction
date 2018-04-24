bddf073b45782da9a8c02d21f1aeb6842962b380
Ben <boriold64@gmail.com>
2018-04-24 15:42:01 -0400

    Minor bugfixing

--- Assets/Scripts/ButtonTextSwitcher.cs
+++ Assets/Scripts/ButtonTextSwitcher.cs
@@ -40,7 +40,8 @@ public class ButtonTextSwitcher : MonoBehaviour {
     }
 
     public void StartMission() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(PlayerInfo.CurrentSceneIndex);
     }
 
 
