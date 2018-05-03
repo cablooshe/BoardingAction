a6172d9fd6fb6fc6b7d52d103b9870bd473b16b9
Andrew Worcester <worcesac@rose-hulman.edu>
2018-05-03 01:59:21 -0400

    set up HighLighted info script
    the script is completely broken and the game crashes now, but that should be fixed first thing tomorrow

---
+++ Assets/HighlightInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighlightInfo : MonoBehaviour {
+
+    public Text squadName;
+    public Text HP;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerSelect.unitsSelected.Count > 0) {
+            squadName.text = PlayerSelect.unitsSelected[0].name;
+            HP.text = PlayerSelect.unitsSelected[0].name;
+            Debug.Log(HP.text);
+        } else {
+            squadName.text = "";
+        }
+	}
+}
