a83bf6115e4bec0858d27b7fdf6ebff421c2ecc9
Gordon Phoon <cablooshe@gmail.com>
2018-05-03 22:52:39 -0400

    temporarily changed playerselect list back to non-static as that breaks selection

--- Assets/HighlightInfo.cs
+++ Assets/HighlightInfo.cs
@@ -17,10 +17,10 @@ public class HighlightInfo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (PlayerSelect.unitsSelected != null) {
-            if (PlayerSelect.unitsSelected.Count > 0) {
-                squadName.text = System.Convert.ToString(PlayerSelect.unitsSelected[0].GetComponent<PUnit>().name);
-                HP.text = System.Convert.ToString(PlayerSelect.unitsSelected[0].GetComponent<PUnit>().currentHealth);
+        if (PlayerSelect.units != null) {
+            if (PlayerSelect.units.Count > 0) {
+                squadName.text = System.Convert.ToString(PlayerSelect.units[0].GetComponent<PUnit>().name);
+                HP.text = System.Convert.ToString(PlayerSelect.units[0].GetComponent<PUnit>().currentHealth);
                 Debug.Log(HP.text);
             } else {
                 squadName.text = "";
--- Assets/__Scripts/PlayerSelect.cs
+++ Assets/__Scripts/PlayerSelect.cs
@@ -38,7 +38,8 @@ public class MouseInfo
 
 public class PlayerSelect : MonoBehaviour {
 
-    public static List<GameObject> unitsSelected;
+    public List<GameObject> unitsSelected;
+    public static List<GameObject> units;
     public bool isSelecting = false;
     public Vector3 mousePosition1;
 
