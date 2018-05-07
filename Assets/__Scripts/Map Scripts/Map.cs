a7c68391e26d8d580ea9c49c33202ad94efba533
soehrejm <soehrejm@rose-hulman.edu>
2018-05-07 15:04:46 -0400

    Sequential Objective Spawning
    Guys, it is now possible to sequentially spawn objectives. It's kinda weird, so feel free to ask questions if something doesn't make sense.

--- Assets/__Scripts/Map Scripts/Map.cs
+++ Assets/__Scripts/Map Scripts/Map.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class Map : MonoBehaviour {
 
     [Header("Set in Inspector")]
-    public int numObjectives;
+    public int[] numObjectives; // Number of objectives per phase
+	public int[] spawnsPerPhase; // These ints should sum to the length of spawns, and there should be as many of these as the number of objectives phases
+	public GameObject[] spawns; // GameObjects to be spawned
 
     [Header("Set Dynamically")]
     public int countComplete = 0;
+	public int objectivePhase = 0;
+	public int lastSpawn = 0;
 
     public void CompletedObjective()
     {
         print("completed obj");
         countComplete++;
-        if (countComplete == numObjectives)
+        if (countComplete == numObjectives[objectivePhase])
             FinishedObjectives();
     }
 
     private void FinishedObjectives()
     {
-        print("You Win!");
+		objectivePhase++;
+		if (objectivePhase == numObjectives.Length) {
+			print ("You Win!");
+		} else {
+			for (int i = 0; i < spawnsPerPhase [objectivePhase-1]; i++) {
+				GameObject spawnedObjective = Instantiate (spawns [lastSpawn]);
+				spawnedObjective.transform.SetParent (this.transform);
+				lastSpawn++;
+			}
+			countComplete = 0;
+		}
     }
 }
