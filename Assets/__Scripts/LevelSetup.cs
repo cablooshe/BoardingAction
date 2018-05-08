9b08719688cc09ced9e4c3c023ca38f4b87a61a2
Gordon Phoon <cablooshe@gmail.com>
2018-05-08 13:59:11 -0400

    map setup changes

--- Assets/__Scripts/LevelSetup.cs
+++ Assets/__Scripts/LevelSetup.cs
@@ -7,16 +7,19 @@ public class LevelSetup : MonoBehaviour {
     public GameObject unitPrefab;   
 	// Use this for initialization
 	void Start () {
-        spawnLoc.Add(new Vector3(32, 8, 0.65f));
+        /*spawnLoc.Add(new Vector3(32, 8, 0.65f));
         spawnLoc.Add(new Vector3(29, 11, 0.65f));
         spawnLoc.Add(new Vector3(24, 12, 0.65f));
-        spawnLoc.Add(new Vector3(28, 10, 0.65f));
+        spawnLoc.Add(new Vector3(28, 10, 0.65f));*/
         IList<Squad> squads = PlayerInfo.Squads;
+        print(squads.Count);
         if(squads == null) {
             return;
         }
         for(int i = 0; i < squads.Count;i++){
             Squad test = squads[i];
+            if (squads[i].leader.data.leaderName.Length ==0)
+                continue;
             SquadLeader leader = test.leader;
             string name = leader.data.leaderName;
             float hp = test.squadHealth;
