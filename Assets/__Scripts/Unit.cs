a65f887dfe8e1b67d273d905778c58bd2f7a9822
Gordon M Phoon <phoongm@rose-hulman.edu>
2018-05-11 16:01:24 -0400

    duh duh duh duh duh duh inspector gadget

--- Assets/__Scripts/Unit.cs
+++ Assets/__Scripts/Unit.cs
@@ -198,7 +198,7 @@ public abstract class Unit : PT_MonoBehaviour {
 
 	public void attackAnimation(GameObject target) {
         for(int i = 0; i < transforms.Count(); i++){ //for every character within a unit
-		    muzzleFlashFront = Instantiate(muzzlePrefab) as GameObject;
+            muzzleFlashFront = Instantiate(muzzlePrefab) as GameObject;
             if (i == 0) {
 		        muzzleFlashFront.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, this.gameObject.transform.position.z-0.5f);
             } else {
