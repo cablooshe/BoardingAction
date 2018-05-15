1e183d1db095eee0957bc537daa40c5ec087c5f8
Jake Douglass <douglaje@rose-hulman.edu>
2018-05-15 15:47:14 -0400

    pRELIMINARY UI Stuff

--- Assets/__Scripts/Map Scripts/Map.cs
+++ Assets/__Scripts/Map Scripts/Map.cs
@@ -8,6 +8,7 @@ public class Map : MonoBehaviour {
     public int[] numObjectives; // Number of objectives per phase
 	public int[] spawnsPerPhase; // These ints should sum to the length of spawns, and there should be as many of these as the number of objectives phases
 	public GameObject[] spawns; // GameObjects to be spawned
+    public bool isPrimary = true;
 
     [Header("Set Dynamically")]
     public int countComplete = 0;
