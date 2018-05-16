71d9be8c267910eef718b88ed706bd976fcb60bc
Gordon M Phoon <phoongm@rose-hulman.edu>
2018-05-16 18:23:46 -0400

    c4 is textured badly but has a nice flashing red light

---
+++ Assets/__Scripts/C4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C4 : MonoBehaviour {
+    private GameObject lightSource;
+    private float time = 0.4f;
+    private bool isActive = false;
+	// Use this for initialization
+	void Start () {
+        lightSource = transform.Find("Spotlight").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > time) {
+            Flip();
+            time += 2;
+        }
+	}
+    void Flip() {
+        lightSource.SetActive(!isActive);
+        isActive = !isActive;
+    }
+}
