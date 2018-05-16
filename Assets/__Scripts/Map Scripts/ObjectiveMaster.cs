eca98e6e3425b426b17c47a4f5ba733457e659f7
Jake Douglass <douglaje@rose-hulman.edu>
2018-05-16 16:46:43 -0400

    ObjectiveMaster class
    All other objectives will submit.

---
+++ Assets/__Scripts/Map Scripts/ObjectiveMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveMaster : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public float numObjectiveHandlers;
+
+    public void decrement()
+    {
+        if (numObjectiveHandlers-- <= 0)
+        {
+            allDone();
+        }
+    }
+
+    public void allDone()
+    {
+        print("YOU WIN!!!!");
+    }
+}
